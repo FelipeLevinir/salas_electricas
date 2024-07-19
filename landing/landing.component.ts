83c62acf0f8a564cea9135db82742a64dd4de5b0
Felipe Leviñir <69121852+FelipeLevinir@users.noreply.github.com>
2024-07-19 16:50:24 -0400

    Add files via upload

---
+++ landing/landing.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LayoutService } from 'src/app/layout/service/app.layout.service';
+
+@Component({
+    selector: 'app-landing',
+    templateUrl: './landing.component.html',
+    styleUrls: ['./landing.component.css']
+})
+export class LandingComponent implements OnInit {
+
+    products = [
+        {
+            name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+            image: 'assets/ingeap/1.jpg'
+        },
+        {
+            name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+            image: 'assets/ingeap/2.jpg'
+        },
+        {
+            name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+            image: 'assets/ingeap/3.jpg'
+        }
+    ];
+    
+    responsiveOptions: any[] = [
+        {
+            breakpoint: '1199px',
+            numVisible: 1,
+            numScroll: 1
+        },
+        {
+            breakpoint: '991px',
+            numVisible: 2,
+            numScroll: 1
+        },
+        {
+            breakpoint: '767px',
+            numVisible: 1,
+            numScroll: 1
+        }
+    ];
+
+    constructor(public layoutService: LayoutService, public router: Router) { }
+
+    ngOnInit() {
+        
+    }
+}
