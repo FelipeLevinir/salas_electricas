9d705902fc1a1d911529673b527c2f0bcba94288
FelipeLevinir <apoyo.desarrollo3@congreso.cl>
2024-08-22 11:10:54 -0400

    avance

--- ingeap-agro/src/app/components/ingeap-agro/menu-servicio-5/menu-servicio-5.component.ts
+++ ingeap-agro/src/app/components/ingeap-agro/menu-servicio-5/menu-servicio-5.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+import { ButtonModule } from 'primeng/button';
+import { AccordionModule } from 'primeng/accordion';
 
 @Component({
   selector: 'app-menu-servicio-5',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent ,FooterComponent, ButtonModule, AccordionModule],
   templateUrl: './menu-servicio-5.component.html',
   styleUrl: './menu-servicio-5.component.scss'
 })
