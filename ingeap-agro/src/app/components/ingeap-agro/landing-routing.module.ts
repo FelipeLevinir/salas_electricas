5d297639999a26e621d7a0197e5573e282f1d1ee
FelipeLevinir <apoyo.desarrollo3@congreso.cl>
2024-08-26 18:52:59 -0400

    avances

--- ingeap-agro/src/app/components/ingeap-agro/landing-routing.module.ts
+++ ingeap-agro/src/app/components/ingeap-agro/landing-routing.module.ts
@@ -7,6 +7,8 @@ import { MenuServicio2Component } from './menu-servicio-2/menu-servicio-2.compon
 import { MenuServicio3Component } from './menu-servicio-3/menu-servicio-3.component';
 import { MenuServicio4Component } from './menu-servicio-4/menu-servicio-4.component';
 import { MenuServicio5Component } from './menu-servicio-5/menu-servicio-5.component';
+import { MenuServicio6Component } from './menu-servicio-6/menu-servicio-6.component';
+import { MenuServicio7Component } from './menu-servicio-7/menu-servicio-7.component';
 
 @NgModule({
     imports: [RouterModule.forChild([
@@ -16,7 +18,9 @@ import { MenuServicio5Component } from './menu-servicio-5/menu-servicio-5.compon
         { path: 'paneles-fotovoltaicos', component: MenuServicio2Component },
         { path: 'monitoreo-extraccion-agua', component: MenuServicio3Component },
         { path: 'monitoreo-riego', component: MenuServicio4Component },
-        { path: 'aranet', component: MenuServicio5Component }
+        { path: 'aranet', component: MenuServicio5Component },
+        { path: 'telemetria', component: MenuServicio6Component },
+        { path: 'control-heladas', component: MenuServicio7Component },
     ])],
     exports: [RouterModule]
 })
