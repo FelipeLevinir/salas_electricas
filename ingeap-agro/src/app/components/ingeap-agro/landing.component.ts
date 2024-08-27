38d9145cce24f74d6bc58c86425e4f8dd04cb63a
FelipeLevinir <apoyo.desarrollo3@congreso.cl>
2024-08-27 13:44:40 -0400

    avance en responsivo

--- ingeap-agro/src/app/components/ingeap-agro/landing.component.ts
+++ ingeap-agro/src/app/components/ingeap-agro/landing.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
-
+import { SocialMediaServiceService } from './services/social-media-service.service';
 @Component({
     selector: 'app-landing',
     templateUrl: './landing.component.html',
@@ -82,10 +82,16 @@ export class LandingIngeapAgroComponent implements OnInit {
         }
     ];
 
-    constructor(public layoutService: LayoutService, public router: Router) { }
+    facebookPosts: any[] = [];
+    instagramPosts: any[] = [];
+    linkedinPosts: any[] = [];
+
+    constructor(public layoutService: LayoutService, public router: Router, public socialMediaService: SocialMediaServiceService) { }
 
     ngOnInit() {
-        
+        this.loadFacebookPosts();
+        this.loadInstagramPosts();
+        this.loadLinkedInPosts();
     }
 
     navigateToFragment(fragment: string) {
@@ -103,4 +109,22 @@ export class LandingIngeapAgroComponent implements OnInit {
     navegarSalas(){
         this.router.navigate(['/pagina-en-construccion']);
     }
+
+    loadFacebookPosts(): void {
+        this.socialMediaService.getFacebookPosts().subscribe(data => {
+          this.facebookPosts = data['data']; // Ajusta según la estructura de la respuesta
+        });
+      }
+    
+      loadInstagramPosts(): void {
+        this.socialMediaService.getInstagramPosts().subscribe(data => {
+          this.instagramPosts = data['data']; // Ajusta según la estructura de la respuesta
+        });
+      }
+    
+      loadLinkedInPosts(): void {
+        this.socialMediaService.getLinkedInPosts().subscribe(data => {
+          this.linkedinPosts = data['elements']; // Ajusta según la estructura de la respuesta
+        });
+      }
 }
